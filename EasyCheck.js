aa2247bcfee240ceee424503b183a5fe501c8730
ray <inthinkcolor@gmail.com>
2013-04-26 12:52:57 +0800

    commit
    commit

---
+++ EasyCheck.js
@@ -0,0 +1,442 @@
+// jQuery EasyCheck Plugin
+//
+// Version 1.9
+//
+// Copy By RAY
+// inthinkcolor@gmail.com
+// 2012
+//
+var EasyCheck={
+	     "loadChk":true, //use onkeyup or onblur?
+	     "blurChk":true,   //use onblur? 
+		"keyupChk":true, //use onkeyup?
+	    "email":/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
+	     "url":/^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
+	    "number":/^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/,
+		"integer":/^\d+$/,
+		"English" : /^[A-Za-z]+$/,
+		"Chinese" :  /^[\u0391-\uFFE5]+$/,
+		"Zip" : /^[1-9]\d{5}$/,
+		"Currency" : /^\d+(\.\d+)?$/,
+		"Require" : /.+/,
+		"ipv4":/^(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)$/i,
+		"ipv6":/^((([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){6}:[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){5}:([0-9A-Fa-f]{1,4}:)?[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){4}:([0-9A-Fa-f]{1,4}:){0,2}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){3}:([0-9A-Fa-f]{1,4}:){0,3}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){2}:([0-9A-Fa-f]{1,4}:){0,4}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){6}((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|(([0-9A-Fa-f]{1,4}:){0,5}:((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|(::([0-9A-Fa-f]{1,4}:){0,5}((\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b)\.){3}(\b((25[0-5])|(1\d{2})|(2[0-4]\d)|(\d{1,2}))\b))|([0-9A-Fa-f]{1,4}::([0-9A-Fa-f]{1,4}:){0,5}[0-9A-Fa-f]{1,4})|(::([0-9A-Fa-f]{1,4}:){0,6}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){1,7}:))$/,
+	   "showError":function(o,msg){  //show error msg
+			var eo = $("[id*='error_"+$(o).attr("name")+"']");
+			if(eo.size()==0){
+				     $(o).after("<div id='error_"+$(o).attr("name")+"'></div>"); //create msg div
+					 eo = $("[id='error_"+$(o).attr("name")+"']");
+			}
+				
+					eo.removeClass();
+					eo.addClass("easycheck_errorInfo");
+				
+					$(o).removeClass("easycheck_okInput");
+					$(o).addClass("easycheck_errorInput");
+					
+					var info =eo.attr("info");
+					if(info){
+					   eo.html(info+msg);
+					}else{
+						  eo.html(msg);
+					}
+					eo.show();
+		
+			
+		},
+		"clearError":function(o,msg){   //OK MSG
+				var eo = $("[id='error_"+$(o).attr("name")+"']");
+				if(eo){
+					eo.removeClass();
+					$(o).removeClass("easycheck_errorInput");
+					$(o).addClass("easycheck_okInput");
+					
+					eo.addClass("easycheck_okInfo");
+					if(msg){
+						eo.html(msg);
+					}else{
+						eo.html("");	
+					}
+					eo.hide();
+								
+			}
+		},
+		"clearAllError":function(){
+			$("[id^='error_']").each(function(){
+				var oName=$(this).attr("id").replace("error_","");
+				var n=$("[name='"+oName+"']");
+				n.removeClass("easycheck_errorInfo");
+				n.addClass("easycheck_okInput");
+			});
+
+			$("[id^='error_']").hide("");
+		},
+		"formatMsg":function(){
+			var ary = [];
+ 			 for(i = 1 ; i < arguments.length ; i++){
+  				 ary.push(arguments[i]);
+		  }
+		   return arguments[0].replace(/\{(\d+)\}/g,function(m ,i){
+  				 return ary[i];
+ 			 });
+		}		,
+			/*
+	Define new validator，invoke EasyCheck.addChkMethod(o,e,chkCode,msg)  to register 
+	o DOM element
+	e event
+	chkCode callback validation function 
+	msg show error message
+
+	*/
+		"addChkMethod":function (o,e,chkCode,msg){
+			if(!chkCode(o)){
+					EasyCheck.showError(o,msg);
+					if(e){
+							e.stopImmediatePropagation(); //stop event pop
+					}
+					return false;
+		  }else{
+				  EasyCheck.clearError(o);
+				 return true;
+		  }
+	}
+	,
+		/*
+			validator Object(validatorName,validatorFunction[,isAttributeValidator])
+			when is a attribute validator ,set isAttributeValidator=true 
+	*/
+	"ChkRule":function(chkName,chkFunction,chkAttr){
+			this.chkName=chkName;
+			this.chkFunction=chkFunction;
+			this.chkAttr=chkAttr;
+	},
+	"chkList":"",  //validator list
+    "msg":{  //validator msg list
+		"required":"Is required",
+		"email":"Invalid email",
+		"url":"Invalid url",
+		"number":"Invalid number",
+		"integer":"Invalid integer",
+		"equalto":"Didn't  match input",
+		"equallength":"length  has to be {0}",
+		"minlength][maxlength":"Please use between {0} and {1} characters",
+		"minlength":"Use at least {0} characters",
+		"maxlength":"Must have at most {0} characters",
+		"min][max":"Value is between{0}and{1}",
+		"min":"The minimum value of {0}", 
+		"max":"The maximum value of {0}",
+		"regexp":"Invalid value",
+		 "extension":"Invalid extension,only {0}",
+		 "vc":"Didn't match the word verification" //verifycode
+
+		},
+		"easyCheckIgnore":{},  
+		"easyCheckBlurIgnore":{},  
+		"easyCheckKeyupIgnore":{},   
+		"easyCheckEleIgnore":{},
+		"easyCheckSubmitDisable":true, 
+		"removeDisableBtn":[], 
+		"removeDisableForm":[],  
+		"removeDisable":false
+}
+/*
+ validator list
+ can register your expand
+*/
+EasyCheck.chkList=[
+ 		new EasyCheck.ChkRule("required",checkRequired),
+	   new EasyCheck.ChkRule("email",checkEmail),
+	   new  EasyCheck.ChkRule("url",checkUrl),
+	   new  EasyCheck.ChkRule("number",checkNumber),
+	   new  EasyCheck.ChkRule("integer",checkInteger),
+	   new  EasyCheck.ChkRule("equalto",checkEqualto,true),
+	   new  EasyCheck.ChkRule("equallength",checkEquallength,true),
+	   new  EasyCheck.ChkRule("minlength][maxlength",checkRangeLength,true),
+	   new  EasyCheck.ChkRule("minlength",checkMinlength,true),
+	   new  EasyCheck.ChkRule("maxlength",checkMaxlength,true),
+	   new  EasyCheck.ChkRule("min][max",checkRange,true),
+	    new  EasyCheck.ChkRule("min",checkMin,true),
+		new  EasyCheck.ChkRule("max",checkMax,true),
+		new  EasyCheck.ChkRule("reg",checkRegExp,true),
+		new  EasyCheck.ChkRule("extension",checkExtension,true),
+		new  EasyCheck.ChkRule("vc",checkVc,true)
+ ];
+ 
+ /*
+  * ignore validator element
+  */  
+  EasyCheck.easyCheckIgnore["vc"]=true;  //vc validator only work on form validation
+  //EasyCheck.easyCheckBlurIgnore["vc"]=true;
+ // EasyCheck.easyCheckKeyupIgnore["vc"]=true;
+ 
+  /*
+  * ignore validator element
+  */  
+  EasyCheck.easyCheckEleIgnore["uservc"]=true; //uservc Element only form validation
+ 
+
+  
+ function checkVc(o,e){
+ 	 return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 var res=false;
+				 $.ajaxSetup({"async":false});
+				 $.post($(o).attr("vc"),$(o).attr("name")+"="+val+"&n="+new Date(),function(d){
+					res=d.replace(/\r\n/g,"");
+				 },"text");
+				 return res=="true";
+			},
+			EasyCheck.msg["vc"]);
+ }
+
+   function checkRegExp(o,e){
+	   return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 var reg= new RegExp('^(?:' + $(o).attr("reg") + ')$');
+				 return !($.trim(val)!=""&&!reg.test(val));
+			},
+			EasyCheck.msg["regexp"]);
+  }
+
+     function checkExtension(o,e){
+		 ex="png,jpeg,jpg,gif";
+	   return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 ex=$(o).attr("extension");
+				 
+				 var extensionList=ex!=""?ex.replace(/,/g, '|'):"png|jpe?g|gif";
+				 return!($.trim(val)!=""&&!val.match(new RegExp(".(" + extensionList + ")$", "i")));
+			},
+				EasyCheck.formatMsg(EasyCheck.msg["extension"],ex));
+  }
+
+
+	function checkRequired(o,e){	
+		
+		return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 
+				 return $.trim(val)!="";
+				
+			},
+			EasyCheck.msg["required"]);
+	}
+
+
+	function checkEmail(o,e){		
+			return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !($.trim(val)!=""&&!EasyCheck.email.test(val));
+			},
+			EasyCheck.msg["email"]);
+
+	}
+	function checkUrl(o,e){
+		return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !($.trim(val)!=""&&!EasyCheck.url.test(val));
+			},
+				EasyCheck.msg["url"]);
+
+	}
+	function  checkNumber(o,e){
+		return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !($.trim(val)!=""&&!EasyCheck.number.test(val));
+			},
+				EasyCheck.msg["number"]);
+	}
+	
+	function checkInteger(o,e){
+		return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !($.trim(val)!=""&&!EasyCheck.integer.test(val));
+			},
+				EasyCheck.msg["integer"]);
+}
+
+function checkEqualto(o,e){
+		return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !(val!=$("[id='"+$(o).attr("equalto")+"']").val());
+			},
+				EasyCheck.msg["equalto"]);
+}
+
+function checkEquallength(o,e){
+		return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !(val.length!=$(o).attr("equallength"));
+			},
+				EasyCheck.formatMsg(EasyCheck.msg["equallength"],$(o).attr("equallength")));
+}
+
+function checkRangeLength(o,e){
+	return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !($.trim(val).length<$(o).attr("minlength")||$.trim(val).length>$(o).attr("maxlength"));
+			},
+			EasyCheck.formatMsg(EasyCheck.msg["minlength][maxlength"],$(o).attr("minlength"),$(o).attr("maxlength")));
+}
+
+function checkMinlength(o,e){
+	return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !($.trim(val).length<$(o).attr("minlength"));
+			},
+			EasyCheck.formatMsg(EasyCheck.msg["minlength"],$(o).attr("minlength")));
+}
+
+
+
+function checkMaxlength(o,e){
+	return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !($.trim(val).length>$(o).attr("maxlength"));
+			},
+		 EasyCheck.formatMsg(EasyCheck.msg["maxlength"],$(o).attr("maxlength")));	
+}
+
+
+function checkRange(o,e){
+	return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !(parseFloat($.trim(val))<parseFloat($(o).attr("min"))||parseFloat($.trim(val))>parseFloat($(o).attr("max"))||isNaN(val));
+			},
+			 EasyCheck.formatMsg(EasyCheck.msg["min][max"],$(o).attr("min"),$(o).attr("max")));	
+}
+function checkMin(o,e){
+	return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !(parseFloat($.trim(val))<parseFloat($(o).attr("min"))||isNaN(val));
+			},
+				 EasyCheck.formatMsg(EasyCheck.msg["min"],$(o).attr("min")));	
+}
+
+function checkMax(o,e){
+		return EasyCheck.addChkMethod(o,e,
+			 function(o){
+				 var val=$(o).val();
+				 return !(parseFloat($.trim(val))>parseFloat($(o).attr("max"))||isNaN(val));
+			},
+			EasyCheck.formatMsg(EasyCheck.msg["min"],$(o).attr("min")));	
+}
+
+
+function chk(o,e,chkFunction){  //callback function
+
+	chkFunction(o,e);
+}
+/*
+registry Class validator and Attribute validator
+*/
+function addChk(chkrule){
+	var chkElements=chkrule.chkAttr?"["+chkrule.chkName+"]":"[class~='"+chkrule.chkName+"']";
+
+	$(chkElements).on("blur",function(e){ 
+			if(!EasyCheck.easyCheckBlurIgnore[chkrule.chkName]&&(!EasyCheck.easyCheckEleIgnore[e.target.name])){
+				EasyCheck.blurChk?chk(this,e,chkrule.chkFunction):'';  
+			}
+		}).on("focus",function(e){
+				
+				  		 EasyCheck.clearError(this);
+					
+				
+		}).on("keyup",function(e){ 
+				if(!EasyCheck.easyCheckKeyupIgnore[chkrule.chkName]&&(!EasyCheck.easyCheckEleIgnore[e.target.name])){
+			 	EasyCheck.keyupChk?chk(this,e,chkrule.chkFunction):'';  
+				}
+		});
+}
+
+
+function addChkForm(chkrule,fromChkInfo){
+			var chkElements=chkrule.chkAttr?"["+chkrule.chkName+"]":"[class~='"+chkrule.chkName+"']";
+			
+			$(fromChkInfo.eleArea+chkElements).each(function(index, element) {
+					   if(!fromChkInfo.errorEleArray[$(element).attr("name")]){  
+							var flag=chkrule.chkFunction(element);
+							if(!flag){  
+								fromChkInfo.errorEleArray[$(element).attr("name")]="E"; 
+								fromChkInfo.chkFlag=false;
+							}
+					   }else{
+					   		fromChkInfo.chkFlag=false;
+					   }
+				 }); 	
+}
+
+
+
+
+	function easyCheck(){
+
+		for(var i=0;i< EasyCheck.chkList.length;i++){
+				var chkrule= EasyCheck.chkList[i];
+				if(!EasyCheck.easyCheckIgnore[chkrule.chkName]){
+					addChk(chkrule);  //callback function
+				}
+		}
+	}
+	
+
+
+/*
+onsubmit,form validation 
+*/
+function easyCheckForm(eleArea){
+				var fromChkInfo={
+					"eleArea":"[id='"+$(eleArea).attr("id")+"'] ", 
+					"chkFlag":true,   
+					"errorEleArray":new Array()
+				};
+			
+
+		
+				for(var i=0;i< EasyCheck.chkList.length;i++){
+					var chkrule= EasyCheck.chkList[i];  
+				    addChkForm(chkrule,fromChkInfo); 
+				}				
+			
+	
+				if(fromChkInfo.chkFlag==true&&EasyCheck['easyCheckSubmitDisable']==true){		
+							$(fromChkInfo.eleArea+":submit").attr("disabled","true");
+				}else{		
+							$(fromChkInfo.eleArea+":submit").removeAttr("disabled");
+				}
+				 return fromChkInfo.chkFlag;
+}
+
+
+$(function(){
+	EasyCheck.loadChk?easyCheck():'';
+
+	for(var i=0;i<EasyCheck.removeDisableBtn.length;i++){
+		$("#"+EasyCheck.removeDisableBtn[i]).removeAttr("disabled");
+	}
+
+	if(EasyCheck.removeDisable==true){
+		$("form :submit").removeAttr("disabled");
+	}else{
+		for(var i=0;i<EasyCheck.removeDisableForm.length;i++){
+			$("[id='"+EasyCheck.removeDisableForm[i]+"'] :submit").removeAttr("disabled");
+		}
+	}
+	
+
+});
---
+++ readme.txt
@@ -0,0 +1,67 @@
+﻿
+A powerful jQuery plugin to validate online forms
+
+EasyCheck supports many validation rules and was created to enforce form processing on the client-side.
+
+This frees up server resources to handle other tasks, while the user's browser does all the heavy lifting.
+
+Notice:the plugin same as EasyCheck,this is English Version,the message and user manual in English.
+
+1、Use EasyCheck
+<link rel="stylesheet" type="text/css" href="css/EasyCheck.css"/>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/EasyCheck.min.js"></script>
+
+
+2、USE Validator
+
+CLASS validator：
+
+required        <input type="text" name="name" class=”required”/>     
+email             <input type="text" name="email" class=”email”/>
+url                   <input type="text" name="url" class=”url”/>
+number          <input type="text" name="sal" class=”number”/>
+integer          <input type="text" name="age" class=”digits”/>
+
+multiple(required email):  <input type="text" name="name" class=”required email”/>
+
+ATTRIBUTE validator:
+equalTo              <input type="password" name="pwd" equalTo="ElementId"/>
+equallength        <input type="password" name="vc"  equallength ="4"/>
+maxlength          <input type="text" name="name" maxlength="20"/>
+minlength           <input type="text" name="name" minlength="6"/>
+max                   <input type="text" name="name" max="20"/>
+min                    <input type="text" name="name" min="2"/>
+min                    <input type="text" name="name" min="2"/>
+extension          <input type="text" name="name" extension=""/>
+reg                     <input type="text" name="name" reg="[A-Z]*"/>
+
+multiple(maxlength minlength),range validator:  
+<input  type="password" value=""  name="urepwd" size="20"  class="txt required"  equalto="upwd" maxlength="12" minlength="6"/>
+
+multiple(min max),range validator:  
+<input  type="password" value=""  name="urepwd" size="20"  class="txt required"  min="20" max="40"/>
+
+
+3、Form Validation ,only set id and onsubmit="return easyCheckForm(this)" 
+  <form action="login.action" onsubmit="return easyCheckForm(this)" id="regForm"> 
+
+
+
+
+4、Add New Validator
+//Define your validator function 
+function checkExists(o,e){
+	return EasyCheck.addChkMethod(o,e,
+		function(o){ 
+			var val=$(o).val();
+			--WRITE YOUR CODE,return the result  is true or false --
+		},
+		"--VALIDATOR MESSAGE--");
+}
+//Register Validator（ValidatorName，validator function ）
+EasyCheck.chkList.push(new EasyCheck.ChkRule("exists",checkExists));
+
+
+
+More use of information, please download《EasyCheck User manual(En)》
